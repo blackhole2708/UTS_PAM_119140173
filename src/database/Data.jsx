1ce504b4fad3d53bd4d64c4527cf7d1883ad5239
119140173.hasiholan <89154087+blackhole2708@users.noreply.github.com>
2022-03-27 11:36:05 +0700

    Add files via upload

---
+++ src/database/Data.jsx
@@ -0,0 +1,44 @@
+const JADWAL = [
+    {
+        jadwal_id: "1",
+        dermaga_keberangkatan: "Belawan",
+        dermaga_tujuan: "Kuala",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "Senin, 21 Maret 2022",
+        jam_masuk: "14.30 WIB"
+    },
+    {
+        jadwal_id: "2",
+        dermaga_keberangkatan: "Belawan",
+        dermaga_tujuan: "Kuala",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "Senin, 21 Maret 2022",
+        jam_masuk: "14.30 WIB"
+    },
+    {
+        jadwal_id: "3",
+        dermaga_keberangkatan: "Belawan",
+        dermaga_tujuan: "Kuala",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "Selasa, 22 Maret 2022",
+        jam_masuk: "17.00 WIB"
+    },
+    {
+        jadwal_id: "4",
+        dermaga_keberangkatan: "Belawan",
+        dermaga_tujuan: "Kuala",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "Rabu, 23 Maret 2022",
+        jam_masuk: "15.00 WIB"
+    },
+    {
+        jadwal_id: "5",
+        dermaga_keberangkatan: "Kuala",
+        dermaga_tujuan: "Belawan",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "Kamis, 24 Maret 2022",
+        jam_masuk: "09.00 WIB"
+    },
+];
+
+export { JADWAL }
